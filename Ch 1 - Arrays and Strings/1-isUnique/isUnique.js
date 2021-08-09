611a9b653693884ab436ebcb1f8ff05b5bab9fb4
Derek <maloned@uci.edu>
2021-08-09 14:31:16 -0700

    Answer tp isUnique

--- Ch 1 - Arrays and Strings/1-isUnique/isUnique.js
+++ Ch 1 - Arrays and Strings/1-isUnique/isUnique.js
@@ -2,6 +2,19 @@
 
 function isUnique(str) {
   //code
+  // with more data structures
+  // use object to store what you find in str
+  // if something found appears again then
+  var obj = {}
+  for (var i = 0; i < str.length; i++) {
+    if (obj[str[i]] != undefined) {
+      return false;
+    } else {
+      obj[str[i]] = 1;
+    }
+  }
+  return true;
+  // without
 }
 
 module.exports = isUnique;
